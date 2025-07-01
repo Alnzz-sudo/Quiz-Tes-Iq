5f04a48cb2ee91511c5dbdd2e486312e5df42b90
ALDY W <114704869+Alnzz-sudo@users.noreply.github.com>
2025-07-01 22:07:26 +0700

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,19 @@
+function adminResetLeaderboard() {
+  const username = prompt("Masukkan username admin:");
+  const password = prompt("Masukkan password admin:");
+
+  // Ubah ini sesuai keinginan lo
+  const adminUsername = "admin";
+  const adminPassword = "aldy120898";
+
+  if (username === adminUsername && password === adminPassword) {
+    const konfirmasi = confirm("Yakin ingin menghapus semua data leaderboard?");
+    if (konfirmasi) {
+      localStorage.removeItem("leaderboard");
+      alert("Leaderboard berhasil di-reset!");
+      location.reload(); // refresh halaman
+    }
+  } else {
+    alert("Akses ditolak! Username atau password salah.");
+  }
+}
