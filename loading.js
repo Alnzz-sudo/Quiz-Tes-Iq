254d8e61798101a16ed3b7553cfc35a5d28f3fe7
ALDY W <114704869+Alnzz-sudo@users.noreply.github.com>
2025-07-02 22:42:40 +0700

    Update loading.js

--- loading.js
+++ loading.js
@@ -2,20 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const startBtn = document.getElementById("startBtn");
   const loadingScreen = document.getElementById("loading-screen");
 
-  console.log("Script jalan...");
-
   if (startBtn && loadingScreen) {
     startBtn.addEventListener("click", () => {
-      console.log("Tombol diklik");
-
       loadingScreen.classList.remove("hidden");
-
       setTimeout(() => {
-        console.log("Redirect ke quiz.html...");
         window.location.href = "quiz.html";
-      }, 1600); // 1.6 detik
+      }, 1600);
     });
-  } else {
-    console.warn("Tombol atau loading screen gak ditemukan!");
   }
 });
